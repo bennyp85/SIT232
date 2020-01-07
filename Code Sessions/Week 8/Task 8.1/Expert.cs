5438e56e49288c7dcf653ed5600829b292e5d2c8
bennyp85 <benjamintp.1985@gmail.com>
2020-01-07 22:55:49 +1100

    Add constructors

--- Code Sessions/Week 8/Task 8.1/Expert.cs
+++ Code Sessions/Week 8/Task 8.1/Expert.cs
@@ -2,9 +2,24 @@ namespace Task_8._1
 {
     public class Expert
     {
-        public Expert(string expertise)
+        public Expert(string field, string project)
         {
-            
+            _Field = field;
+            _Project = project;
+        }
+
+        private string _Field;
+        public string Field
+        {
+            get => _Field;
+            set => _Field = value;
+        }
+
+        private string _Project;
+        public string Project
+        {
+            get => _Project;
+            set => _Project = value;
         }
     }
 }
