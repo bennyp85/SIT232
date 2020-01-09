9f68845d41144d201caa2d0316106f2f9380898a
bennyp85 <benjamintp.1985@gmail.com>
2020-01-09 20:31:18 +1100

    Removed inherited classes

--- Code Sessions/Week 8/Task 8.1/Expert.cs
+++ Code Sessions/Week 8/Task 8.1/Expert.cs
@@ -2,17 +2,25 @@ namespace Task_8._1
 {
     public class Expert
     {
-        public Expert(string field, string project)
+        public Expert(string techField, string pubField, string project)
         {
-            _Field = field;
+            _TechField = techField;
+            _PubField = pubField;
             _Project = project;
         }
 
-        private string _Field;
-        public string Field
+        private string _TechField;
+        public string TechField
         {
-            get => _Field;
-            set => _Field = value;
+            get => _TechField;
+            set => _TechField = value;
+        }
+
+        private string _PubField;
+        public string PubField
+        {
+            get => _PubField;
+            set => _PubField = value;
         }
 
         private string _Project;
@@ -21,5 +29,8 @@ namespace Task_8._1
             get => _Project;
             set => _Project = value;
         }
+
+        public override string ToString() => $"{_TechField}, {_PubField}";
+    
     }
 }
--- Code Sessions/Week 8/Task 8.1/Program.cs
+++ Code Sessions/Week 8/Task 8.1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task_8._1
 {
@@ -6,8 +7,34 @@ namespace Task_8._1
     {
         static void Main(string[] args)
         {
-            TechEx techEx1 = new TechEx("programming");
-            PublishingEx publishingEx1 = new PublishingEx("editor");
+            List<Expert> ExpertList = new List<Expert>();
+            Console.WriteLine("Enter # of Experts: ");
+            int numOfExperts = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < numOfExperts; i++)
+            {
+                Console.WriteLine("Enter Tech Field: ");
+                string techField = Console.ReadLine();
+                Console.WriteLine("Enter Pub Field: ");
+                string pubField = Console.ReadLine();
+                ExpertList.Add(new Expert(techField, pubField, null));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Experts have the following technical expertise:");
+            // Print list of Expert fields
+            foreach (var VARIABLE in ExpertList)
+            {
+                Console.WriteLine(VARIABLE.TechField);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Experts have the following publishing expertise:");
+            
+            foreach (var VARIABLE in ExpertList)
+            {
+                Console.WriteLine(VARIABLE.PubField);
+            }
             
         }
     }
